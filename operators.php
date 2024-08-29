4407e76a859c4774e172f22e32a19ed721d9e0e3
AnjasFedo <fedoaf98@gmail.com>
2024-08-29 11:39:10 +0700

    6. Operators

---
+++ operators.php
@@ -0,0 +1,57 @@
+<?php
+
+// arithmetic operators
+// addition
+echo 2 + 4;
+
+// subtraction
+echo 4 - 2;
+
+// multiplication
+echo 4 * 2;
+
+// division
+echo 4 / 2;
+
+// modulus
+echo 4 % 2;
+
+// exponentiation   
+echo 4 ** 2;
+
+// comparison
+// equal
+var_dump(2 == 2);
+var_dump(2 == '2');
+var_dump(2 === '2');
+
+// not equal
+var_dump(2 != 2);
+var_dump(2 !== '2');
+
+// greater than and less than
+var_dump(2 > 2);
+var_dump(2 < 2);
+
+var_dump(2 >= 2);
+var_dump(2 <= 2);
+
+// logic operator
+// and
+var_dump(true and true);
+var_dump(true and false);
+var_dump(false and false);
+
+// or
+var_dump(true or true);
+var_dump(true or false);
+var_dump(false or false);
+
+// not
+var_dump(!true);
+var_dump(!false);
+
+// xor
+var_dump(true xor true);
+var_dump(true xor false);
+var_dump(false xor false);
