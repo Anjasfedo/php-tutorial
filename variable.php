246688072496b001895170f5ffcd5fda994149fb
AnjasFedo <fedoaf98@gmail.com>
2024-08-29 11:20:35 +0700

    4. Variables

---
+++ variable.php
@@ -0,0 +1,34 @@
+<?php
+
+// structure ($name = value;)
+$example = 10;
+echo $example;
+
+// correct name (alphabets, numbers and _)
+$example_name1 = 10;
+
+// correct first letter (no numbers)
+$name = 1;
+$Name = 1;
+$_name = 1;
+
+// not correct
+// $3example = 1;
+
+// case ($NAME vs $name)
+$example = 10;
+$examplE = 11;
+echo $example;
+echo $examplE;
+
+// type of value
+// Int num = 10; // on static lang
+$num = 10; // dynamic lang
+echo $num;
+
+// assign new value
+$x = 10;
+echo $x;
+
+$x = 'lorem';
+echo $x;
